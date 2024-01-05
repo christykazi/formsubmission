048b3c6e1dca8c6186563e5615579cddc78c44a9
christykazi <christyagbai4178@gmail.com>
2024-01-06 00:30:31 +0100

    my first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <Form>
+    <h1>Contact <span>Here</span></h1>
+    <input type="text" name='name' id='id' />
+    <h1>Contact <span>Here</span></h1>
+    <input type="text" name='name' id='id' />
+    <h1>Contact <span>Here</span></h1>
+    <input type="text" name='name' id='id' />
+    </Form>
+  )
+}
+
+export default Form
