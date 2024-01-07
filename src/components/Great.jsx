c03559639d027e0634a6c0020d75e0bf585ffc4c
christykazi <christyagbai4178@gmail.com>
2024-01-07 22:20:00 +0100

    form submission completed

--- src/components/Great.jsx
+++ src/components/Great.jsx
@@ -4,16 +4,36 @@ import { useState } from 'react'
 
 const Great = () => {
   const [data,setData] = useState({name: "", email: "", phone: "", message: ""})
-  return (
-    
-    <form>
-    <h1>Contact <span>Here</span></h1>
-    <input type="text" name='name' id='' placeholder='Full Name' />
+const handleChange = (e) => {
+const  name = e.target.name;
+const value = e.target.value;
+setData({...data, [name]: value})
+}
 
-    <input type="email" name='email' id='id' placeholder='example@gmail.com' />
+const handleSubmit = (e) => {
+e.preventDefault()
+console.log(data)
+}
+
+  return ( 
+    <form method='post' onSubmit={handleSubmit}>
+    <h1>Contact <span>Here</span></h1>
+    <input type="text" name='name' id='' 
+    onChange={handleChange} value={data.name} placeholder='Full Name' />
+
+    <input type="email" name='email' id='id' 
+    onChange={handleChange} 
+    value={data.email}
+    placeholder='example@gmail.com' />
     
-    <input type="phone" name='phone' id='id' placeholder='+234'/>
-    <textarea name="message" id="" cols="30" rows="10" placeholder='type here...'/>
+    <input type="phone" name='phone' id='id'
+    onChange={handleChange}
+    value={data.phone}
+     placeholder='+234' required/>
+    <textarea name="message" id="" cols="30" rows="10" 
+    onChange={handleChange}
+    value={data.message}
+    placeholder='type here...'/>
     <button type='submit'>Send</button>
     </form>
     
