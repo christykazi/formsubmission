71d95b601f86f3dc3eaa6a7014948b4193af9ac0
christykazi <christyagbai4178@gmail.com>
2024-01-07 19:53:04 +0100

    updating great page

--- src/components/Great.jsx
+++ src/components/Great.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 
 const Great = () => {
+  const [data,setData] = useState({name: "", email: "", phone: "", message: ""})
   return (
     
-    <div>
+    <form>
     <h1>Contact <span>Here</span></h1>
-    <input type="text" name='name' id='' />
+    <input type="text" name='name' id='' placeholder='Full Name' />
 
-    <input type="email" name='email' id='id' />
+    <input type="email" name='email' id='id' placeholder='example@gmail.com' />
     
-    <input type="phone" name='phone' id='id' />
-    <textarea name="message" id="" cols="30" rows="10"/>
+    <input type="phone" name='phone' id='id' placeholder='+234'/>
+    <textarea name="message" id="" cols="30" rows="10" placeholder='type here...'/>
     <button type='submit'>Send</button>
-    </div>
+    </form>
     
   )
 }
